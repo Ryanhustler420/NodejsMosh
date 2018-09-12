1dd412a52269291dc9f4d1327544f58cf154f86b
Gaurav Gupta <gouravgupta840@gmail.com>
2018-09-12 21:33:45 +0530

    Set Envirnoment variable in nodejs
    Using export PORT=5000 command

--- BasicExpressApi/index.js
+++ BasicExpressApi/index.js
@@ -5,6 +5,16 @@ app.get('/',(req,res) => {
   res.send('Hello World');
 });
 
-app.listen(3000,() => {
-  console.log("Server is up and running!");
+app.get('/api/courses',(req,res) => {
+  res.send([1,2,3,4]);
+});
+
+
+// PORT
+var port = (process.env.PORT || 3000);
+// to manually assign port you can use terminal and type this command
+// export PORT=5000, and you are good to go!
+
+app.listen(port,() => {
+  console.log(`Server is up and running on port ${port} ...`);
 });
