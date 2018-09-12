1724ffaa772341b96c88c57908ec84c7873dd562
Gaurav Gupta <gouravgupta840@gmail.com>
2018-09-12 21:40:46 +0530

    Route Parameters

--- BasicExpressApi/index.js
+++ BasicExpressApi/index.js
@@ -9,6 +9,23 @@ app.get('/api/courses',(req,res) => {
   res.send([1,2,3,4]);
 });
 
+// /api/courses/1
+app.get('/api/courses/:id/:year',(req,res) => {
+
+  // http://localhost:5000/api/courses/2/2018?sortBy=name
+  // res.send(req.params);
+  // {
+  //   "id": "2",
+  //   "year": "2018"
+  // }
+
+  //http://localhost:5000/api/courses/2/2018?sortBy=name
+  // res.send(req.query);
+  // {
+  // "sortBy": "name"
+  // }
+
+});
 
 // PORT
 var port = (process.env.PORT || 3000);
