6e60139b40b674d9e5952a276e5058a02530b8bd
Gaurav Gupta <gouravgupta840@gmail.com>
2018-09-12 21:52:12 +0530

    Handling HTTP GET Requests

--- BasicExpressApi/index.js
+++ BasicExpressApi/index.js
@@ -1,37 +1,30 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+  {id:1,name:'BBA'},
+  {id:2,name:'BCA'},
+  {id:3,name:'MBA'}
+]
+
+
 app.get('/',(req,res) => {
   res.send('Hello World');
 });
 
 app.get('/api/courses',(req,res) => {
-  res.send([1,2,3,4]);
+  res.send(courses);
 });
 
-// /api/courses/1
-app.get('/api/courses/:id/:year',(req,res) => {
-
-  // http://localhost:5000/api/courses/2/2018?sortBy=name
-  // res.send(req.params);
-  // {
-  //   "id": "2",
-  //   "year": "2018"
-  // }
-
-  //http://localhost:5000/api/courses/2/2018?sortBy=name
-  // res.send(req.query);
-  // {
-  // "sortBy": "name"
-  // }
-
+app.get('/api/courses/:id',(req,res) => {
+  let result = courses.find(course => course.id === +req.params.id);
+  if(!result) res.status(404).send('Course not found!');
+  res.send(result);
 });
 
-// PORT
+
+
 var port = (process.env.PORT || 3000);
-// to manually assign port you can use terminal and type this command
-// export PORT=5000, and you are good to go!
-
 app.listen(port,() => {
   console.log(`Server is up and running on port ${port} ...`);
 });
