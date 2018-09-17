8e693a14c199d8497d36ccc95d34771b3cd98cf9
Gaurav Gupta <gouravgupta840@gmail.com>
2018-09-17 17:00:51 +0530

    Pagination

--- CRUD Operations Using MongoDB/index.js
+++ CRUD Operations Using MongoDB/index.js
@@ -37,12 +37,18 @@ async function createCourse(){
 // createCourse();
 
 async function getCourses(){
+
+  const pageNumber = 2;
+  const pageSize = 10;
+
+  // /api/courses?pageNumber=2&pageSize=10
+
   const courses = await Course
   .find({name:'Node.js Course,',isPublished:true})
-  .limit(10)
+  .skip((pageNumber - 1) * pageSize)
+  .limit(pageSize)
   .sort({name: 1})  
-  // .select({name:1,tags:1});
-  .count();
+  .select({name:1,tags:1});
   console.log(courses);
 }
 
