d55f99413e3b36c866a9404058623ae9cbf822fc
Gaurav Gupta <gouravgupta840@gmail.com>
2018-09-17 12:27:32 +0530

    Creating Schema

--- CRUD Operations Using MongoDB/index.js
+++ CRUD Operations Using MongoDB/index.js
@@ -4,3 +4,15 @@ mongoose.connect('mongodb://localhost:27017/playgroundMosh', { useNewUrlParser: 
 .then(conn => {
     console.log(`connected with playgroundMosh Database`);
 }).catch(err => console.log);
+
+// defining schema
+
+const courseSchema = new mongoose.Schema({
+  name:String,
+  author:String,
+  tags:[String],
+  date:{
+    type:Date,default:Date.now
+  },
+  isPublished:Boolean
+});
