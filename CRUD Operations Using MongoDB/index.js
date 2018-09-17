1330287d97a5813144adff21dd150b164a25a443
Gaurav Gupta <gouravgupta840@gmail.com>
2018-09-17 16:35:37 +0530

    Comparison Query Operators

--- CRUD Operations Using MongoDB/index.js
+++ CRUD Operations Using MongoDB/index.js
@@ -34,11 +34,25 @@ async function createCourse(){
   console.log(result);
 }
 
+// createCourse();
 
 async function getCourses(){
-  const courses = await Course.find({
-    name:'Node.js Course'
-  }).limit(10)
+
+  // Compairison Operator
+  // eq (equal)
+  // ne (not equal)
+  // gt (greater than)
+  // gte (greater than or equal to)
+  // lt (less than)
+  // ite (less than or equal to)
+  // in
+  // nin (non in)
+
+  const courses = await Course
+  // .find({name:'Node.js Course'})
+  // .find({price:{ $gt: 10 , $lte: 20}})
+  .find({price:{ $in:[10,15,20] }})
+  .limit(10)
   .sort({name: 1})
   .select({name:1,tags:1});
   console.log(courses);
@@ -47,4 +61,3 @@ async function getCourses(){
 getCourses();
 
 
-// createCourse();
