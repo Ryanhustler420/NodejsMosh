c1cfa1b0b5ab7b66e45798a13b4787aaec99955c
Gaurav Gupta <gouravgupta840@gmail.com>
2018-09-20 10:04:41 +0530

    Referencing Documents In Practice

--- Mongoose- Modeling Relationship Between Connected Data/x.js
+++ Mongoose- Modeling Relationship Between Connected Data/x.js
@@ -1,32 +1,53 @@
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/playground')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
 
-// Using References (Normalization) -> CONSISTENCY
+const Author = mongoose.model('Author', new mongoose.Schema({
+  name: String,
+  bio: String,
+  website: String
+}));
 
-let author = {
-    name : 'Gaurav'
+const Course = mongoose.model('Course', new mongoose.Schema({
+  name: String,
+  author :{
+      type:mongoose.Schema.Types.ObjectId,
+      ref:'Author'
+  }
+}));
+
+async function createAuthor(name, bio, website) { 
+  const author = new Author({
+    name, 
+    bio, 
+    website 
+  });
+
+  const result = await author.save();
+  console.log(result);
 }
 
-let course = {
-    author: 'id'
+async function createCourse(name, author) {
+  const course = new Course({
+    name, 
+    author
+  }); 
+  
+  const result = await course.save();
+  console.log(result);
 }
 
-// Using Embedded Documents (Denormalization) -> PERFORMANCE
-let course = {
-    author:{
-        name:'Gaurav'
-    }
+async function listCourses() { 
+  const courses = await Course
+    .find()
+    .select('name');
+  console.log(courses);
 }
 
-// Hybrid
+// createAuthor('Mosh', 'My bio', 'My Website');
 
-let author = {
-    name: 'Gaurav',
-    //50 other properties
-}
+createCourse('Node Course', '5ba3226b8396a81488111562')
 
-let course = {
-    author: {
-        id:'ref',
-        name:'Gauarv'
-    }
-}
+// listCourses();
