c0eec59141c2485affafbc8a0551940df8fe914c
Gaurav Gupta <gouravgupta840@gmail.com>
2018-09-20 15:17:48 +0530

    Embedded Document Vidly app Mosh Apporach

--- Mongoose- Modeling Relationship Between Connected Data/vidly/models/customer.js
+++ Mongoose- Modeling Relationship Between Connected Data/vidly/models/customer.js
@@ -1,34 +1,34 @@
-const mongoose = require('mongoose');
 const Joi = require('joi');
+const mongoose = require('mongoose');
 
-const Customer = mongoose.model('Customer',new mongoose.Schema({
-    isGold:{
-        type:Boolean,
-        default: false
-    },
-    name:{
-        type:String,
-        required:true,
-        minlength:3,
-        maxlength:50
-    },
-    phone:{
-        type:Number,
-        required:true,
-        minlength:7,
-        maxlength:10
-    }
+const Customer = mongoose.model('Customer', new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  },
+  isGold: {
+    type: Boolean,
+    default: false
+  },
+  phone: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  }
 }));
 
 function validateCustomer(customer) {
-    const schema = {
-        isGold:Joi.boolean(),
-        name: Joi.string().min(3).max(50).required(),
-        phone: Joi.number().integer()
-    };
-  
-    return Joi.validate(customer, schema);
+  const schema = {
+    name: Joi.string().min(5).max(50).required(),
+    phone: Joi.string().min(5).max(50).required(),
+    isGold: Joi.boolean()
+  };
+
+  return Joi.validate(customer, schema);
 }
 
-exports.Customer = Customer;
+exports.Customer = Customer; 
 exports.validate = validateCustomer;
--- Mongoose- Modeling Relationship Between Connected Data/vidly/models/genre.js
+++ Mongoose- Modeling Relationship Between Connected Data/vidly/models/genre.js
@@ -1,26 +1,25 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
 
-const genraSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        minlength:5,
-        maxlength:15 
-    }
-})
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  }
+});
 
-const Genre = mongoose.model('Genre',genraSchema);
+const Genre = mongoose.model('Genre', genreSchema);
 
 function validateGenre(genre) {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
 
-    return Joi.validate(genre, schema);
+  return Joi.validate(genre, schema);
 }
 
-exports.Genre = Genre;
-exports.GenreSchema = genraSchema;
+exports.genreSchema = genreSchema;
+exports.Genre = Genre; 
 exports.validate = validateGenre;
-  
