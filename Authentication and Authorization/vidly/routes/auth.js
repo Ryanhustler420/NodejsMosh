eb47dc4681cb7aefbd199b49ced76a87c22374ea
Gaurav Gupta <gouravgupta840@gmail.com>
2018-09-24 18:26:13 +0530

    Added Authentication Route to vidly app

---
+++ Authentication and Authorization/vidly/routes/auth.js
@@ -0,0 +1,34 @@
+const _ = require('lodash');
+const bcryptjs = require('bcryptjs');
+const {User} = require('../models/user');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+
+router.post('/',async (req,res) => {
+
+    const {error} = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let user = await User.findOne({email: req.body.email});
+    if(!user) return res.status(400).send('Invalid email or password.');
+
+    const validPassword = await bcryptjs.compare(req.body.password,user.password);
+    if(!validPassword) return res.status(400).send('Invalid email or password.');
+
+    res.send(true);
+});
+
+
+function validate(req) {
+    const schema = {
+      email: Joi.string().min(5).max(255).email({ minDomainAtoms: 2 }).required(),
+      password: Joi.string().min(5).max(255).regex(/^[a-zA-Z0-9]{3,30}$/).required()
+    };
+  
+    return Joi.validate(req, schema);
+  }
+
+
+module.exports = router;
