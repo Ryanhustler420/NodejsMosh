1bcf7764e97fede0b02241cc88b7ebf464810add
Gaurav Gupta <gouravgupta840@gmail.com>
2018-09-24 18:42:45 +0530

    testing Auth Route before JWT npm

--- Authentication and Authorization/vidly/routes/auth.js
+++ Authentication and Authorization/vidly/routes/auth.js
@@ -1,10 +1,9 @@
-const _ = require('lodash');
 const bcryptjs = require('bcryptjs');
+const Joi = require('Joi');
 const {User} = require('../models/user');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
-
 
 router.post('/',async (req,res) => {
 
@@ -20,12 +19,11 @@ router.post('/',async (req,res) => {
     res.send(true);
 });
 
-
 function validate(req) {
     const schema = {
       email: Joi.string().min(5).max(255).email({ minDomainAtoms: 2 }).required(),
       password: Joi.string().min(5).max(255).regex(/^[a-zA-Z0-9]{3,30}$/).required()
-    };
+};
   
     return Joi.validate(req, schema);
   }
