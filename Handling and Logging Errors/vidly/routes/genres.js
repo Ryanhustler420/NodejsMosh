53e1380b94db58926b083d4542a6abc3f2f2597a
Gaurav Gupta <gouravgupta840@gmail.com>
2018-09-25 23:32:53 +0530

    installed npm express-async-errors package for route error

--- Handling and Logging Errors/vidly/routes/genres.js
+++ Handling and Logging Errors/vidly/routes/genres.js
@@ -1,4 +1,3 @@
-const asyncMiddleware = require('../middleware/async');
 const admin = require('../middleware/admin');
 const auth = require('../middleware/auth');
 const {Genre, validate} = require('../models/genre');
@@ -6,12 +5,12 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', asyncMiddleware(async (req, res) => {
+router.get('/', async (req, res) => {
   const genres = await Genre.find().sort('name');
   res.send(genres);
-}));
+});
 
-router.post('/', auth, asyncMiddleware(async (req, res) => {
+router.post('/', auth, async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -19,7 +18,7 @@ router.post('/', auth, asyncMiddleware(async (req, res) => {
   await genre.save();
   
   res.send(genre);
-}));
+});
 
 router.put('/:id', auth, async (req, res) => {
   const { error } = validate(req.body); 
