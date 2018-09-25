7fed5e91235fd33f160b4ecdc89dee3657476c8d
Gaurav Gupta <gouravgupta840@gmail.com>
2018-09-25 16:20:57 +0530

    Express Error Middleware for error handling

---
+++ Handling and Logging Errors/vidly/hash.js
@@ -0,0 +1,9 @@
+const bcryptjs = require('bcryptjs');
+
+async function run(){
+    const salt = await bcryptjs.genSalt(10);
+    const hashed = await bcryptjs.hash('1234',salt);
+    console.log(hashed);
+} 
+
+run();
---
+++ Handling and Logging Errors/vidly/routes/rentals.js
@@ -0,0 +1,64 @@
+const auth = require('../middleware/auth');
+const {Rental, validate} = require('../models/rental'); 
+const {Movie} = require('../models/movie'); 
+const {Customer} = require('../models/customer');
+const Fawn = require('fawn');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+Fawn.init(mongoose);
+
+router.get('/', async (req, res) => {
+  const rentals = await Rental.find().sort('-dateOut');
+  res.send(rentals);
+});
+
+// CI 5ba39ed592f78722f89a3130
+// MI 5ba39e5c92f78722f89a312e
+
+router.post('/', auth, async (req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  
+  const customer = await Customer.findById(req.body.customerId);
+  if (!customer) return res.status(400).send('Invalid customer.');
+
+  const movie = await Movie.findById(req.body.movieId);
+  if (!movie) return res.status(400).send("Invalid movie.");
+
+  if(movie.numberInStock === 0) return res.status(400).send('Movie not in stock.');
+
+  let rental = new Rental({ 
+    customer:{
+        _id:customer._id,
+        name: customer.name,
+        isGold:customer.isGold,
+        phone:customer.phone,
+    },
+    movie:{
+        _id:movie._id,
+        title: movie.title,
+        dailyRentalRate: movie.dailyRentalRate
+    }
+  });
+
+  try{
+    new Fawn.Task()
+      .save('rentals',rental)
+      .update('movies',{ _id : movie._id},{
+          $inc:{ numberInStock: -1 }
+      })
+      .run();
+  
+    res.send(rental);
+  }catch(e){
+    res.status(500).send('Something failed.');
+  }
+});
+
+module.exports = router; 
+
+
+
